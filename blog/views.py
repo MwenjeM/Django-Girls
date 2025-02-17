afbfeef1f0fcd8fb8e52ff1f9bcec501ede2c07d
MwenjeM <78741625+MwenjeM@users.noreply.github.com>
2025-02-16 23:58:23 -0500

    Updated Django URLs, Views, Templates, and ORM functionality

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post  # Import Post model
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()  # Retrieve all posts from database
+    return render(request, 'blog/post_list.html', {'posts': posts})
