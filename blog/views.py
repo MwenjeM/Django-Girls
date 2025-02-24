17599aaf10d9311b0cc675a1502de9e7293472ae
MwenjeM <78741625+MwenjeM@users.noreply.github.com>
2025-02-23 23:39:22 -0500

    Dynamic data, templates, CSS and Template extending

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Post  # Import Post model
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()  # Retrieve all posts from database
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
